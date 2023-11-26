4602693418b27f047d68ee6cf3a70e520a8a153c
Karim-308 <2001118@eng.asu.edu.eg>
2023-11-26 23:32:21 +0200

    ADDED login page gui

--- Elcapitano/src/main/java/com/example/elcapitano/HelloApplication.java
+++ Elcapitano/src/main/java/com/example/elcapitano/HelloApplication.java
@@ -12,9 +12,11 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        fxmlLoader.setControllerFactory(c -> new HelloController());
+        Scene scene = new Scene(fxmlLoader.load(), 600, 400);
+        stage.setTitle("Login!");
         stage.setScene(scene);
+        stage.setResizable(false);
         stage.show();
     }
 
--- Elcapitano/src/main/java/com/example/elcapitano/HelloController.java
+++ Elcapitano/src/main/java/com/example/elcapitano/HelloController.java
@@ -1,9 +1,29 @@
 package com.example.elcapitano;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.CheckBox;
 import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
+
+import java.util.Objects;
+import java.util.concurrent.atomic.AtomicReferenceFieldUpdater;
 
 public class HelloController {
+/*
+    @FXML
+    private ImageView imageView;
+
+    @FXML
+    private void initialize() {
+        Image image = new Image(getClass().getClassLoader().getResourceAsStream("/assets/test_Logo.png"));
+        imageView.setImage(image);
+    }
+*/
+
     @FXML
     private Label welcomeText;
 
@@ -11,4 +31,29 @@ public class HelloController {
     protected void onHelloButtonClick() {
         welcomeText.setText("Welcome to JavaFX Application!");
     }
+
+
+    /* PASSWORD FIELDS PART IN LOGIN SCREEN */
+
+    @FXML
+    private PasswordField passwordField;
+
+    @FXML
+    private CheckBox showPasswordCheckBox;
+
+    @FXML
+    private void updateDisplay() {
+        if (showPasswordCheckBox.isSelected()) {
+            passwordField.setPromptText(passwordField.getText());
+            passwordField.setText("");
+            passwordField.setDisable(true);
+        } else {
+            passwordField.setDisable(false);
+            passwordField.setText(passwordField.getPromptText());
+            /*passwordField.setPromptText("");*/
+        }
+    }
+    /* END OF  PASSWORD FIELDS PART IN LOGIN SCREEN  */
+
+
 }
