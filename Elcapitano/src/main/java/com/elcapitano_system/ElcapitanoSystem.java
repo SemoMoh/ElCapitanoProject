d8dad808d52fd18de1042e06fc25a29b49f77d0c
Ahmed Bakr <114930002+AhmedBakrXI@users.noreply.github.com>
2023-12-09 01:53:37 +0200

    Update ElcapitanoSystem.java

--- Elcapitano/src/main/java/com/elcapitano_system/ElcapitanoSystem.java
+++ Elcapitano/src/main/java/com/elcapitano_system/ElcapitanoSystem.java
@@ -1,14 +1,21 @@
 package com.elcapitano_system;
 
 import com.backend.accounts.AccountDB;
+import com.backend.accounts.ExpensesSheet;
+import com.backend.accounts.IncomeSheet;
+import com.backend.accounts.Report;
+import java.io.IOException;
+
 
 public class ElcapitanoSystem {
     public static AccountDB accountDB;
+    public static IncomeSheet incomeSheet;
+    public static ExpensesSheet expensesSheet;
+    public static Report report;
 
-    public static void SystemLoad(){
+    public static void SystemLoad() throws IOException {
 
         // initialize the accounts.
         accountDB = AccountDB.getInstance();
-
     }
 }
