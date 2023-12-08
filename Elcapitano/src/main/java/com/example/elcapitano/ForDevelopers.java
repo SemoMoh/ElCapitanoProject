a61e718468dbb565f022674562d4ee00b70a9da9
Karim-308 <2001118@eng.asu.edu.eg>
2023-12-08 14:13:25 +0200

    sucess login for devopers and unsucess alert

--- Elcapitano/src/main/java/com/example/elcapitano/ForDevelopers.java
+++ Elcapitano/src/main/java/com/example/elcapitano/ForDevelopers.java
@@ -2,6 +2,7 @@ package com.example.elcapitano;
 
 import com.elcapitano_system.DevsLogin;
 import javafx.event.ActionEvent;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 
@@ -11,20 +12,33 @@ public class ForDevelopers {
     public TextField devPassword;
     public TextField pathtoDatabase;
     public Button confirmDatabase;
-    private String pathtoDatabse;
-    
-    
 
-    public String getPathtoDatabse() {
-        return pathtoDatabse;
-    }
+    private String successPathtoDatabase;
 
     public void confirmDatabase(ActionEvent actionEvent) {
         DevsLogin d = HelloApplication.d;
-        String username,password,path;
-        // get strings from text fields
+        String username, password, path;
 
-        boolean successLogin = d.newLogin(username,password,path);
-        
+        // get strings from text fields
+        username = devusername.getText();
+        password = devPassword.getText();
+        path = pathtoDatabase.getText();
+
+        boolean successLogin = d.newLogin(username, password, path);
+
+        if (successLogin) {
+            successPathtoDatabase = path;
+        } else {
+            // Show an alert for incorrect credentials
+            showAlert("Wrong Developer Credentials", "The provided credentials are incorrect. Please try again.");
+        }
+    }
+
+    private void showAlert(String title, String content) {
+        Alert alert = new Alert(Alert.AlertType.ERROR);
+        alert.setTitle(title);
+        alert.setHeaderText(null);
+        alert.setContentText(content);
+        alert.showAndWait();
     }
 }
