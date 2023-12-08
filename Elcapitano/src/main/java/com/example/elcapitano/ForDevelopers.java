34f29d48dc0624d381b358eeb4710a78e5cc8115
Karim-308 <2001118@eng.asu.edu.eg>
2023-12-08 13:40:21 +0200

    pathtoDatabase developers Screen

---
+++ Elcapitano/src/main/java/com/example/elcapitano/ForDevelopers.java
@@ -0,0 +1,17 @@
+package com.example.elcapitano;
+
+import javafx.event.ActionEvent;
+
+public class ForDevelopers {
+    
+    private String pathtoDatabse;
+    
+    
+
+    public String getPathtoDatabse() {
+        return pathtoDatabse;
+    }
+
+    public void confirmDatabase(ActionEvent actionEvent) {
+    }
+}
