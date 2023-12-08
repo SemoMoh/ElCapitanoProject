8a1075d8c102251b29db0de2cc5b847952e5b2ca
SemoMoh <128331219+SemoMoh@users.noreply.github.com>
2023-12-08 14:23:56 +0200

    update devLogin

--- Elcapitano/src/main/java/com/example/elcapitano/ForDevelopers.java
+++ Elcapitano/src/main/java/com/example/elcapitano/ForDevelopers.java
@@ -2,9 +2,14 @@ package com.example.elcapitano;
 
 import com.elcapitano_system.DevsLogin;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class ForDevelopers {
 
@@ -15,7 +20,7 @@ public class ForDevelopers {
 
     private String successPathtoDatabase;
 
-    public void confirmDatabase(ActionEvent actionEvent) {
+    public void confirmDatabase(ActionEvent actionEvent) throws IOException {
         DevsLogin d = HelloApplication.d;
         String username, password, path;
 
@@ -28,6 +33,16 @@ public class ForDevelopers {
 
         if (successLogin) {
             successPathtoDatabase = path;
+
+            // For a successful login, open the accounts log in and continue the app as normal.
+            FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+            fxmlLoader.setControllerFactory(c -> new HelloController());
+            Scene scene = new Scene(fxmlLoader.load(), 600, 400);
+            Stage stage = new Stage();
+            stage.setTitle("Login!");
+            stage.setScene(scene);
+            stage.setResizable(false);
+            stage.show();
         } else {
             // Show an alert for incorrect credentials
             showAlert("Wrong Developer Credentials", "The provided credentials are incorrect. Please try again.");
--- Elcapitano/src/main/java/com/example/elcapitano/HelloApplication.java
+++ Elcapitano/src/main/java/com/example/elcapitano/HelloApplication.java
@@ -26,17 +26,16 @@ public class HelloApplication extends Application {
             stage.setScene(scene);
             stage.setResizable(false);
             stage.show();
-        }
-
-
+        } else{
        /* this.newStage =stage;*/
-       /* FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        fxmlLoader.setControllerFactory(c -> new HelloController());
-        Scene scene = new Scene(fxmlLoader.load(), 600, 400);
-        stage.setTitle("Login!");
-        stage.setScene(scene);
-        stage.setResizable(false);
-        stage.show();*/
+            FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+            fxmlLoader.setControllerFactory(c -> new HelloController());
+            Scene scene = new Scene(fxmlLoader.load(), 600, 400);
+            stage.setTitle("Login!");
+            stage.setScene(scene);
+            stage.setResizable(false);
+            stage.show();
+        }
     }
 
     public static void main(String[] args) {
